478858bab9967602e4a380313a810a1c70bf215e
William Russell <russellwp@g.cofc.edu>
2017-02-13 22:08:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/william/mortgage_calculator_project_russell/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.william.mortgage_calculator_project_russell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Scanner reader = new Scanner(System.in);
+    }
+}
---
+++ app/src/test/java/com/example/william/mortgage_calculator_project_russell/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.william.mortgage_calculator_project_russell;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
